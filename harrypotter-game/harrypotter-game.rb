cacdf545d7bb9a9c762625493bc9b97bfd765c92
irahey <iraolivia.garcia@yahoo.com>
2015-09-07 20:20:04 -0400

    Finished the Harry potter cli game

--- harrypotter-game/harrypotter-game.rb
+++ harrypotter-game/harrypotter-game.rb
@@ -1,89 +1,96 @@
 # Learned this from http://rmion.com/ruby-newbie-programming-my-first-text-game/
- 
-def select_weapon
-	# Store weapons in new array
-	weapons = ['sword', 'staff', 'bow', 'hammer', 'axe']
- 
-	# Ask user to choose a starting weapon
-	puts "Choose your weapon by typing its associated number: "
- 
-	# Loop through weapons array and print options to console
-	# As items are added to or removed from array
-	# this loop always prints a string containing a number (starting from 1)
-	# and the value in the weapons array at that number
-	(0...weapons.length).each do |i|
-		puts "#{i+1} - #{weapons[i]}"
-	end
-	print "> "
-	# store choice in variable
-	# [MUST RESOLVE] currently, if user enters letter, it is somehow mapping to a number
-	choice = $stdin.gets.chomp.to_i - 1
-	@selected_weapon = weapons[choice]
- 
-	puts "You selected: #{@selected_weapon}"
-	start_game
-end
- 
 def start_game
-	# Assign initial integer values for each character's health
-	@player_health = 50
-	@monster_health = 50
-	# Ask player if they are ready to play
-	puts "\nReady to play? Y/N"
+
+	puts"\nWelcome, one and all, to Hogwarts School of Witchcraft and Wizardry!"
+	puts "Today is the Sorting Ceremony. The Sorting Hat will determine whish Hogwarts House you will belong."
+	puts "\nAre you ready to be sorted? Y/N"
 	print "> "
 	
 	confirm = $stdin.gets.chomp.upcase
 	if confirm == 'Y'
-		enter_room
+		sorting
 	else
 		puts "Game over"
+		exit(0)
 	end
-end
+end 
  
-def enter_room
-	# Inform user of current status
-	puts "You awake in a small room."
-	puts "There is a large door in the distance that is locked."
-	puts "In front of you is a small table."
-	puts "On that table is a coin.\n\n"
-	coin_toss
-end
- 
-def coin_toss
+def sorting
 	puts "You pick up the coin and toss it in the air."
-	# Generate random number between 0 and 2
-	# and round down to either 0 or 1
-	# store resulting integer in variable coin
-	coin = rand(2).floor
-	# Ask user to pick a side of the coin
-	print "\nPick a side (heads or tails):\n> "
-	choice = $stdin.gets.chomp.upcase
- 
-	# if-statement to determine whether user guessed coin side correctly
-	# [MUST RESOLVE] feels very repetitive, try other programming mechanism
-	if choice.include?('H') && coin == 0
-		puts "It's heads! Nice job!"
-		treasure_room
-	elsif choice.include?('H') && coin == 1
-		puts "Sorry, it's tails. Try again."
-		coin_toss
-	elsif choice.include?('T') && coin == 0
-		puts "Sorry, it's heads. Try again."
-		coin_toss
-	elsif choice.include?('T') && coin == 1
-		puts "It's tails! Nice job!"
-		treasure_room
+
+	coin = rand(4).floor
+
+	if coin == 0
+		puts "Gryffindor!"
+		@house = "Gryffindor"
+	elsif coin == 1
+		puts "Hufflepuff!"
+		@house = "Hufflepuff"
+	elsif coin == 2
+		puts "Ravenclaw!"
+		@house = "Ravenclaw"
+	elsif coin == 3
+		puts "Slytherin!"
+		@house = "Slytherin"
 	end
+	puts "\nCongratulations! You now belong to #{@house} House."
+	friends
 end
  
-def treasure_room
-	puts "\nThe door in front of you opens."
-	puts "You walk forward, #{@selected_weapon} at the ready."
-	puts "Passing through the doorway, an object catches your gaze."
-	puts "It's a large treasure chest, likely filled with riches."
-	puts "But from the darkness appears a monstrous creature."
-	puts "This creature is heavily armored and carries a large hammer."
-	puts "The only way to reach the chest is to dispose of the creature."
+def friends
+	puts "Friends are really important to survive wizarding school! Time to make friends."
+	friends = ['Ron Weasley', 'Neville Longbottom','Luna Lovegood', 'Hagrid', 'Argus Filch', 'Cedric Diggory', 'Cho Chang']
+	puts "Please choose which one you want to attempt to make friends with:"
+	(0...friends.length).each do |i|
+		puts "#{i+1} - #{friends[i]}"
+	end
+	print "> "
+	choice = $stdin.gets.chomp.to_i - 1
+	@selected_friend = friends[choice]
+	puts "You selected: #{@selected_friend}"
+	attempt_friend
+end
+
+def attempt_friend
+	puts "Let's see if it's going to work out."
+	attempt = rand(3).floor
+
+	if attempt == 0
+		puts "It did not work out with #{@selected_friend}."
+		puts "Do you want to [A]try again or [B]choose another person or [C]you don't want to attempt making friends anymore?: [A or B or C] ?"
+		print "> "
+		if attempt == 'A'
+			attempt_friend
+		elsif attempt == 'B'
+			friends
+		else
+			puts "That's fine but you're going to be alone at meal time."
+			puts "Also... You won't have anyone to help you when you're in trouble."
+			@player_health = 40
+			@aragog_health = 40
+		end
+	elsif attempt == 1
+		puts "Success! You are now friends with #{@selected_friend}."
+		@player_health = 55
+		@aragog_health = 40
+		puts "\nSo you and your friend #{@selected_friend} went outside in the forests, trying to explore the grounds."
+		puts "\nReady to go outside? Y/N"
+		print "> "
+		
+		confirm = $stdin.gets.chomp.upcase
+		if confirm == 'Y'
+			forbidden_forest
+		else
+			puts "Please take you time resting."
+			exit(0)
+		end
+	end 
+    
+end
+def forbidden_forest
+	puts "\nYou and your friend #{@selected_friend} went outside in the forests, trying to explore the grounds."
+    puts "And little did you know, you have found yourselves at the Forbidden Forest, where Aragog is!"
+    puts "Aragog emerged!"
 	puts "\nAre you prepared to fight? Y/N"
 	choice = $stdin.gets.chomp.upcase
 	if choice == 'Y'
@@ -92,31 +99,27 @@ def treasure_room
 		death
 	end
 end
- 
+
 def attack_monster
-	# Generate random number between 0 and 6
-	# and round down to either 0 or 5
-	# store resulting integer in variable roll
+
 	player_roll = rand(6).floor
  
 	player_damage = player_roll * 5
 	puts "\nYou attack monster for #{player_damage} damage."
  
-	@monster_health -= player_damage
+	@aragog_health -= player_damage
  
-	if @monster_health <= 0
+	if @aragog_health <= 0
 		puts "\nYou defeated the monster!"
-		treasure_chest
+		victory
 	else
-		puts "Monster's health: #{@monster_health}hp"
+		puts "Monster's health: #{@aragog_health}hp"
 		defend_monster
 	end
 end
  
 def defend_monster
-	# Generate random number between 0 and 6
-	# and round down to either 0 or 5
-	# store resulting integer in variable roll
+
 	monster_roll = rand(6).floor
  
 	monster_damage = monster_roll * 5
@@ -138,42 +141,23 @@ def defend_monster
 	end
 end
  
-def treasure_chest
-	puts "\nYou slowly approach the treasure chest."
-	puts "The lid is so heavy it requires the weight of your entire body to lift."
-	puts "As you raise the lid a bright light shines from within the chest."
-	puts "You look down to gaze upon the chest's contents."
-	puts "\nDo you take a closer look? Y/N"
-	choice = $stdin.gets.chomp.upcase
-	if choice == 'Y'
-		victory
-	else
-		puts "You've come this far, don't you want to see what's inside? Y/N"
-		choice = $stdin.gets.chomp.upcase
-		if choice == 'Y'
-			victory
-		else
-			puts "Fine. You die."
-			exit(0)
-		end
-	end
-end
  
 def victory
-	puts "Congratulations. It was a flashlight."
+	puts "Congratulations! You have defeated Aragog."
+	puts "Now you time to go back to Hogwarts! It's almost dinnertime!"
 	exit(0)
 end
  
 def death
-	puts "The monster pwned you."
+	puts "The Aragog defeated you."
 	puts "Game over."
 	puts "\nPlay again? Y/N"
 	choice = $stdin.gets.chomp.upcase
 	if choice == 'Y'
-		select_weapon
+		start_game
 	else
 		exit(0)
 	end
 end
- 
-select_weapon
+
+start_game
